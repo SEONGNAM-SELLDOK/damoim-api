8672715cd62fd479d92cd638de456a0af44b13e0
Incheol Jung <incheol.jung@navercorp.com>
2021-02-13 02:36:57 +0900

    add test

---
+++ src/main/java/com/damoim/restapi/MainController.java
@@ -0,0 +1,19 @@
+package com.damoim.restapi;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**  * MainController
+ *
+ * @author incheol.jung
+ * @since 2021. 02. 13.
+ */
+@RestController
+public class MainController {
+	@GetMapping
+	public ResponseEntity get(){
+		return new ResponseEntity("hello", HttpStatus.ACCEPTED);
+	}
+}
