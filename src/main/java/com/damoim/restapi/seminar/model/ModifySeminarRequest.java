a31dea8483a56c4258d9b094bd22ae5a1b7b1014
Gogisung <ggs0707@naver.com>
2021-02-22 23:13:10 +0900

    쓰기, 읽기, 수정 model

---
+++ src/main/java/com/damoim/restapi/seminar/model/ModifySeminarRequest.java
@@ -0,0 +1,36 @@
+package com.damoim.restapi.seminar.model;
+
+import com.damoim.restapi.seminar.entity.Address;
+import com.damoim.restapi.seminar.entity.DamoimTag;
+import lombok.*;
+
+import javax.persistence.Embedded;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Getter
+@ToString
+@AllArgsConstructor
+@Builder
+public class ModifySeminarRequest {
+    @NotBlank
+    private String title; // 세미나 제목
+    @NotBlank
+    private String content; // 상세 내용
+    @NotBlank
+    private String image; // 대표 이미지
+    @NotNull
+    private String country;
+    @NotNull
+    private String city;
+    @NotNull
+    private String street;
+    @NotNull
+    private String subject; // 주제
+    @NotNull
+    private DamoimTag damoimTag; // 태그
+    @NotNull
+    private LocalDateTime endDate; // 마감일
+}
