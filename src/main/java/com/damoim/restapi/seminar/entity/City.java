68072b2f02763b812552173df93ee262126bed88
Gogisung <ggs0707@naver.com>
2021-02-22 23:12:11 +0900

    국가 코드

---
+++ src/main/java/com/damoim/restapi/seminar/entity/City.java
@@ -0,0 +1,7 @@
+package com.damoim.restapi.seminar.entity;
+/**
+ * @author Gogisung
+ */
+public enum City {
+    seoul, busan, daegu, gwangju, ulsan
+}
---
+++ src/main/java/com/damoim/restapi/seminar/entity/Country.java
@@ -0,0 +1,8 @@
+package com.damoim.restapi.seminar.entity;
+
+/**
+ * @author Gogisung
+ */
+public enum Country {
+    EN, AF, AX, AL, KR, CN, JP
+}
