68efc50074bdff51d2f298cbdc57fa45e4e2cef8
jaejoon <dkansk924@naver.com>
2021-02-27 00:34:26 +0900

    [edit] 시큐리티 의존성추가로 인한 컨트롤러 테스트 실패 수정

---
+++ src/test/java/com/damoim/restapi/secondhandtrade/SecurityConfiguration.java
@@ -0,0 +1,20 @@
+package com.damoim.restapi.secondhandtrade;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.csrf().disable()
+        .authorizeRequests()
+        .antMatchers("/login").authenticated()
+        .anyRequest().permitAll();
+  }
+}
