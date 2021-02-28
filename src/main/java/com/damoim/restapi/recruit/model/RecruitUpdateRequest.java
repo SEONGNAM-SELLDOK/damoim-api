171d16977b717d1cef1afa06f67cb7442a26f544
Lokie89 <traeuman@gmail.com>
2021-02-28 22:11:44 +0900

    chore : #12-recruit duplicate issue

--- src/main/java/com/damoim/restapi/recruit/model/RecruitUpdateRequest.java
+++ src/main/java/com/damoim/restapi/recruit/model/RecruitUpdateRequest.java
@@ -3,8 +3,7 @@ package com.damoim.restapi.recruit.model;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
-import javax.validation.constraints.*;
-import java.time.LocalDate;
+import javax.validation.constraints.NotNull;
 
 /**
  * @author SeongRok.Oh
@@ -15,41 +14,8 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @Setter
 @Getter
-public class RecruitUpdateRequest {
+public class RecruitUpdateRequest extends RecruitSaveRequest {
     @ApiModelProperty(example = "1")
     @NotNull
     private Long id;
-
-    @ApiModelProperty(value = "등록자", required = true, example = "오성록")
-    @NotEmpty
-    private String register;
-
-    @ApiModelProperty(value = "회사", required = true, example = "네이버")
-    @NotEmpty
-    private String company;
-
-    @ApiModelProperty(value = "제목", required = true, example = "새로운 서비스를 함께 할 팀원을 모집합니다.")
-    @NotEmpty
-    private String title;
-
-    @ApiModelProperty(value = "근무지", example = "판교역")
-    @NotBlank
-    private String location;
-
-    @ApiModelProperty(value = "채용보상금", example = "500000")
-    @NotNull
-    @Min(0)
-    private Integer reward;
-
-    @ApiModelProperty(value = "태그", example = "[\"복지좋은회사\",\"인센티브\",\"포털회사\",\"트래픽많은\"]")
-    private String[] tags;
-
-    @ApiModelProperty(value = "내용", example = "B2B 서비스 고도화/안정화를 위하여 백엔드 개발자를 채용하고 있습니다!")
-    private String description;
-
-    @ApiModelProperty(value = "마감일", required = true, example = "2021-02-28")
-    @Future
-    @NotNull
-    private LocalDate deadline;
-
 }
