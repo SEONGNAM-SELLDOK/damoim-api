c65a347e6b4501205b3afa105154df10d1356226
jaejoon <dkansk924@naver.com>
2021-03-05 00:04:21 +0900

    [edit] Code Style

--- src/main/java/com/damoim/restapi/RestApiApplication.java
+++ src/main/java/com/damoim/restapi/RestApiApplication.java
@@ -1,12 +1,8 @@
 package com.damoim.restapi;
 
-import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-import javax.persistence.EntityManager;
 
 @EnableJpaAuditing
 @SpringBootApplication
