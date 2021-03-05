2996db4c4aa3991afe7f441b8a5096a2e0ffa31e
jaejoon <dkansk924@naver.com>
2021-03-05 13:24:06 +0900

    [edit] fileutil

--- src/main/java/com/damoim/restapi/config/fileutil/DamoimFileUtil.java
+++ src/main/java/com/damoim/restapi/config/fileutil/DamoimFileUtil.java
@@ -1,17 +1,11 @@
-package com.damoim.restapi.config.fileutil;
+package com.damoim.restapi.config;
 
-import com.damoim.restapi.config.fileutil.error.FileNotSaveException;
-import com.damoim.restapi.config.fileutil.error.NotSupportedTypeException;
-import com.damoim.restapi.config.fileutil.model.RequestFile;
+import com.damoim.restapi.config.fileutil.FileProperties;
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
-import java.util.UUID;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,27 +13,22 @@ import org.springframework.web.multipart.MultipartFile;
 /**
  * @author gisung go
  * @since 2021-02-22
- */
+ * */
 @Component
 @RequiredArgsConstructor
 public class DamoimFileUtil {
 
-    private final FileProperties fileProperties;
+    private  final FileProperties fileProperties;
 
-
-    @Deprecated
     public String upload(MultipartFile file) {
-
-        fileValidation(file);
-
         Path rootLocation = Paths.get(fileProperties.getFinalPath())
-            .toAbsolutePath().normalize();
+                .toAbsolutePath().normalize();
         String filename = StringUtils.cleanPath(file.getOriginalFilename());
         File convFile;
 
         try {
             Files.createDirectories(rootLocation);
-            Path targetPath = rootLocation.resolve(filename).normalize();
+            Path targetPath =  rootLocation.resolve(filename).normalize();
             convFile = new File(String.valueOf(targetPath));
             file.transferTo(convFile);
         } catch (Exception e) {
@@ -47,42 +36,5 @@ public class DamoimFileUtil {
             return null;
         }
         return fileProperties.getPathLast() + file.getOriginalFilename();
-    }
-
-    public String upload(RequestFile requestFile) {
-        MultipartFile file = requestFile.getFile();
-
-        fileValidation(file);
-
-        Path rootLocation = Paths.get(fileProperties.getFinalPath() + requestFile.getRoot())
-            .toAbsolutePath().normalize();
-        String filename = getFilename(file);
-        File convFile;
-
-        try {
-            Files.createDirectories(rootLocation);
-            Path targetPath = rootLocation.resolve(filename).normalize();
-            convFile = new File(String.valueOf(targetPath));
-            file.transferTo(convFile);
-        } catch (Exception e) {
-            throw new FileNotSaveException(
-                "[File Not Save] file name:" + file.getOriginalFilename());
-        }
-        return fileProperties.getPathLast() + requestFile.getRoot() + filename;
-    }
-
-    private String getFilename(MultipartFile file) {
-        return StringUtils.cleanPath(UUID.randomUUID() + file.getOriginalFilename());
-    }
-
-    private void fileValidation(MultipartFile file) throws NotSupportedTypeException {
-        String contentType = file.getContentType();
-        List<String> supType = List
-            .of(MediaType.IMAGE_PNG_VALUE, MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_GIF_VALUE);
-
-        if (!supType.contains(contentType)) {
-            throw new NotSupportedTypeException(contentType, supType,
-                HttpStatus.UNSUPPORTED_MEDIA_TYPE.value());
-        }
     }
 }
