d017f8fa857382c1f51e50538be2ca1357d98887
Incheol Jung <incheol.jung@navercorp.com>
2021-03-10 01:42:39 +0900

    update logic

--- src/main/java/com/damoim/restapi/member/controller/MemberController.java
+++ src/main/java/com/damoim/restapi/member/controller/MemberController.java
@@ -1,18 +1,23 @@
 package com.damoim.restapi.member.controller;
 
-import com.damoim.restapi.member.dao.MemberMapper;
-import com.damoim.restapi.member.entity.Member;
-import com.damoim.restapi.member.model.GetMemberResponse;
-import com.damoim.restapi.member.model.SaveMemberRequest;
-import com.damoim.restapi.member.model.SaveMemberResponse;
-import com.damoim.restapi.member.service.MemberService;
-import io.swagger.annotations.Api;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.damoim.restapi.member.dao.MemberMapper;
+import com.damoim.restapi.member.model.GetMemberResponse;
+import com.damoim.restapi.member.model.SaveMemberRequest;
+import com.damoim.restapi.member.model.SaveMemberResponse;
+import com.damoim.restapi.member.model.TestUser;
+import com.damoim.restapi.member.service.MemberService;
+import io.swagger.annotations.Api;
+import lombok.RequiredArgsConstructor;
 
 /**
  * Controller
@@ -35,7 +40,7 @@ public class MemberController {
 
     @GetMapping
     public ResponseEntity<GetMemberResponse> get(
-            @AuthenticationPrincipal Member member
+        @AuthenticationPrincipal TestUser member
     ) {
         GetMemberResponse response = new GetMemberResponse();
         BeanUtils.copyProperties(member, response, GetMemberResponse.class);
