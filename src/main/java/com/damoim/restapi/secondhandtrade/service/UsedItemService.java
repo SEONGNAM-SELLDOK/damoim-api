4a0512f920ef3af6d06fcbb2f9fe39cf898bd29d
jaejoon <dkansk924@naver.com>
2021-03-09 13:05:30 +0900

    [edit]

--- src/main/java/com/damoim/restapi/secondhandtrade/service/UsedItemService.java
+++ src/main/java/com/damoim/restapi/secondhandtrade/service/UsedItemService.java
@@ -2,6 +2,7 @@ package com.damoim.restapi.secondhandtrade.service;
 
 
 import com.damoim.restapi.config.fileutil.DamoimFileUtil;
+import com.damoim.restapi.config.fileutil.model.RequestFile;
 import com.damoim.restapi.secondhandtrade.dao.UsedItemRepository;
 import com.damoim.restapi.secondhandtrade.dao.UsedItemSearchRepository;
 import com.damoim.restapi.secondhandtrade.entity.useditem.UsedItem;
@@ -19,7 +20,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
@@ -31,9 +31,9 @@ public class UsedItemService {
   private final DamoimFileUtil damoimFileUtil;
   private final ModelMapper modelMapper;
 
-  public ResponseUsedItem save(UsedItemRequest request, MultipartFile file) {
+  public ResponseUsedItem save(UsedItemRequest request, RequestFile file) {
     UsedItem item = request.toEntity();
-    if (file != null) {
+    if (file.getFile() != null) {
       String upload = damoimFileUtil.upload(file);
       item.setTitleImg(upload);
     }
