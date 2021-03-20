1fd7106c90d2fbcf5877f58c9d517096c0827b2d
Gogisung <ggs0707@naver.com>
2021-03-20 15:14:55 +0900

    swagger 커뮤니티 ApiModelProperty 적용

--- src/main/java/com/damoim/restapi/community/model/ListCommunityResponse.java
+++ src/main/java/com/damoim/restapi/community/model/ListCommunityResponse.java
@@ -1,6 +1,7 @@
 package com.damoim.restapi.community.model;
 
 import com.querydsl.core.annotations.QueryProjection;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,10 +19,15 @@ import java.time.LocalDateTime;
 @Builder
 @NoArgsConstructor
 public class ListCommunityResponse {
+    @ApiModelProperty(example = "1")
     private long id;
+    @ApiModelProperty(value = "제목", example = "1년차 연봉이 궁금합니다.")
     private String title;
+    @ApiModelProperty(value = "내용", example = "자세한 정보는 블라인드 또는 잡플래닛에서 검색해보세요.")
     private String content;
+    @ApiModelProperty(value = "작성일", example = "2021-02-21T20:17:22.137Z")
     private LocalDateTime createDate;
+    @ApiModelProperty(value = "수정일", example = "2021-03-16T20:17:22.137Z")
     private LocalDateTime updateDate;
 
     @QueryProjection
--- src/main/java/com/damoim/restapi/community/model/ModifyCommunityRequest.java
+++ src/main/java/com/damoim/restapi/community/model/ModifyCommunityRequest.java
@@ -1,6 +1,7 @@
 package com.damoim.restapi.community.model;
 
 import com.damoim.restapi.community.entity.Community;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 import javax.validation.constraints.NotBlank;
@@ -17,8 +18,10 @@ import javax.validation.constraints.NotBlank;
 @AllArgsConstructor
 public class ModifyCommunityRequest {
     @NotBlank
+    @ApiModelProperty(value = "제목", example = "1년차 연봉이 궁금합니다.")
     private String title;
     @NotBlank
+    @ApiModelProperty(value = "내용", example = "자세한 정보는 블라인드 또는 잡플래닛에서 검색해보세요.")
     private String content;
 
     public Community updateTo(Community community) {
