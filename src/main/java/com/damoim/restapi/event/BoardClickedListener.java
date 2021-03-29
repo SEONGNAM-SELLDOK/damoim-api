a81701f9dba2c39e8a14f946569cae78a7cd2ae6
JAEJOON KIM <64793712+KJJ924@users.noreply.github.com>
2021-03-29 16:16:21 +0900

    Board count (#59)
    * [add] boardCount
    
    * [add] Thread Max size 1
    
    * [edit] test 수정.
    
    * [edit] code smell
    
    * [edit] Multi Thread
    
    Co-authored-by: Incheol Jung <bluesky761@naver.com>

---
+++ src/main/java/com/damoim/restapi/event/BoardClickedListener.java
@@ -0,0 +1,35 @@
+package com.damoim.restapi.event;
+
+import com.damoim.restapi.event.boardcount.dao.BoardCountRepository;
+import com.damoim.restapi.event.boardcount.entity.BoardCount;
+import java.util.Objects;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Async
+@Component
+@RequiredArgsConstructor
+public class BoardClickedListener {
+
+    private final BoardCountRepository boardCountRepository;
+
+
+    @EventListener
+    @Transactional
+    public synchronized void handleBoardClickedEvent(BoardClickedEvent clickedEvent) {
+        BoardCount boardCount = boardCountRepository
+            .findByBoardIdAndBoardType(clickedEvent.getBoardId(), clickedEvent.getBoardType());
+        if (Objects.isNull(boardCount)) {
+            boardCountRepository.save(clickedEvent.toEntity());
+        }
+
+        if (Objects.nonNull(boardCount)) {
+            boardCountRepository
+                .updateCount(clickedEvent.getBoardId(), clickedEvent.getBoardType());
+        }
+    }
+}
