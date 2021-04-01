57e949f399798d8c66da2d0a4554a11681e98f05
Incheol Jung <incheol.jung@navercorp.com>
2021-04-02 00:21:39 +0900

    update

--- src/main/java/com/damoim/restapi/config/web/WebConfig.java
+++ src/main/java/com/damoim/restapi/config/web/WebConfig.java
@@ -5,6 +5,7 @@ import java.time.format.DateTimeFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -15,6 +16,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedMethods("*")
+			.allowedOriginPatterns("http://localhost:3000")
+			.allowCredentials(true);
+	}
 
 	@Override
 	public void addFormatters(FormatterRegistry registry) {
